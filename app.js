decc4f043bad6769cc25882105e72a0228f41563
Jasvaran Mann <mannjesse355@gmail.com>
2024-02-12 17:50:01 -0800

    added hosting url to cors

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://blog-api-9r0h.onrender.com'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://blog-api-9r0h.onrender.com','http://localhost:3000'],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
 }))
